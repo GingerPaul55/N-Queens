32d32313895682d8e6676c8b7acda99405e7ad05
Paul Hutchinson <PaulHappyHutchinson@gmail.com>
2017-12-03 23:54:17 +0000

    Add basic optimizer for boards 19 or greater
    It's mostly to set the groundwork for a better optimizer.

--- nqueens.php
+++ nqueens.php
@@ -1,6 +1,8 @@
 <?php
-// N-Queens problem: put N Queens on a chess board NxN
-// sized such that they aren't at risk of capture
+/**
+ * N-Queens problem: put N Queens on a chess board NxN
+ * sized such that they aren't at risk of capture.
+ */
 class Queens
 {
     /**
@@ -21,9 +23,19 @@ class Queens
      * @var bool[][]
      */
     private $used = [
+        'row' => [],
         'column' => [],
         'down' => [],
         'up' => [],
+    ];
+
+    /**
+     * @var int[]
+     */
+    private $stats = [
+        'assignments' => 0,
+        'backtracks' => 0,
+        'queens-placed' => 0,
     ];
 
     public function __construct(int $size)
@@ -32,8 +44,34 @@ class Queens
         $this->result = array_fill(0, $size, null);
     }
 
-    public function solve(int $queenNum = 0, $row = 0): bool
+    public function optimize()
     {
+        if ($this->size < 19) {
+            return;
+        }
+
+        $this->assign($this->size - 1, 0, true);
+        $this->assign($this->size - 2, 2, true);
+        $this->assign($this->size - 3, 4, true);
+        $this->assign($this->size - 4, 1, true);
+        $this->assign($this->size - 5, 3, true);
+    }
+
+    public function solve()
+    {
+        return $this->doSolve(count($this->used['row']), 0);
+    }
+
+    private function doSolve(int $queenNum, int $row): bool
+    {
+        if (($this->used['row'][$row] ?? false) === true) {
+            if(($queenNum === $this->size - 1)
+                || $this->doSolve($queenNum + 1, $row + 1) === true
+            ) {
+                return true;
+            }
+            return false;
+        }
         for($col = 0; $col < $this->size; $col++) {
             $down = $this->size - 1 - $row + $col;
             $up = $row + $col;
@@ -47,33 +85,38 @@ class Queens
             }
 
             // if this cell is allowed, set the queen here
-            $this->result[$row] = $col;
-            $this->used['column'][$col] = true;
-            $this->used['down'][$down] = true;
-            $this->used['up'][$up] = true;
-
+            $this->assign($row, $col, true);
 
             // If last queen or subsequent queens have been placed, return
             if(($queenNum === $this->size - 1)
-                || $this->solve($queenNum + 1, $row + 1) === true
+                || $this->doSolve($queenNum + 1, $row + 1) === true
             ) {
                 return true;
             }
 
             // otherwise, if we get here we've backtracked and have to try replacing this queen
-            $this->result[$row] = null;
+            $this->stats['backtracks']++;
 
-            $this->used['column'][$col] = false;
-            $this->used['down'][$down] = false;
-            $this->used['up'][$up] = false;
+            $this->assign($row, $col, false);
         }
 
         return false;
     }
 
+    private function assign(int $row, int $col, bool $value): void
+    {
+        $this->stats['assignments']++;
+        $this->result[$row] = $col;
+        $this->used['row'][$row] = $value;
+        $this->used['column'][$col] = $value;
+        $this->used['down'][$this->size - 1 - $row + $col] = $value;
+        $this->used['up'][$row + $col] = $value;
+    }
+
     // Rudimentary printing method
     public function display()
     {
+        $this->stats['queens-placed'] = 0;
         for($row = 0; $row < $this->size; $row++)
         {
             $sep = '-';
@@ -81,17 +124,23 @@ class Queens
             {
                 $sep .= '----';    // for every column add 4 dashes to then print below the row
                 echo '| ';
-
+                $this->stats['queens-placed'] += (int)($this->result[$row] === $col);
                 echo $this->result[$row] === $col ? 'Q ' : '  ';
             }
 
             echo "|\r\n";
             echo $sep . "\n";    // print the seperator row -------
         }
+
+        echo '== Stats ==', PHP_EOL;
+        foreach ($this->stats as $key => $value) {
+            echo $key, ': ', number_format($value), PHP_EOL;
+        }
     }
 }
 
 // Run main ...
 $queens = new Queens($_SERVER['argv'][1]);
+$queens->optimize();
 $queens->solve();
 $queens->display();
