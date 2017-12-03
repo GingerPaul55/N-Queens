3aba283c30db57eb3bf1a5fe70946b1bbca91996
Paul Hutchinson <PaulHappyHutchinson@gmail.com>
2017-12-03 22:56:08 +0000

    Optimize and refactor whole file + PHP 7.2ify
    Each column and diagonal (one for each direction) now has a "used" flag, this prevents doing a board scan when trying to find out if a Queen can be placed at a position.
    
    For example; using the same algorithm for a 20 queens on my dev laptop, yields 
    Before:
    real	0m 8.70s
    user	0m 8.69s
    sys	0m 0.00s
    
    After: 
    real	0m 0.97s
    user	0m 0.96s
    sys	0m 0.00s

--- nqueens.php
+++ nqueens.php
@@ -1,100 +1,97 @@
 <?php
-
-// N-Queens problem: put N Queens on a chess board NxN 
+// N-Queens problem: put N Queens on a chess board NxN
 // sized such that they aren't at risk of capture
-
 class Queens
 {
-	private $board = array();
-	private $numQueens = 8;
-	
-	public function __construct($n = 8)
-	{
-		$this->numQueens = $n;
-		
-		// Fill the board with 0s
-		for($i = 0; $i < $n; $i++)
-		{
-			$this->board[$i] = array_fill(0, $n, 0);
-		}
-	}
-	
-	function solve($queenNum, $row)
-	{
-		for($col = 0; $col < $this->numQueens; $col++)
-		{
-			if($this->allowed($row, $col))
-			{
-				// if this cell is allowed, set the queen here
-				$this->board[$row][$col] = 1;
-				
-				// If last queen or subsequent queens have been placed, return
-				if(($queenNum === $this->numQueens - 1) || $this->solve($queenNum + 1, $row + 1) === true) return true;
-				
-				// otherwise, if we get here we've backtracked and have to try replacing this queen
-				$this->board[$row][$col] = 0;
-			}
-		}
-		
-		return false;
-	}
-	
-	function allowed($x, $y)
-	{
-		$n = $this->numQueens;
-		
-		// Only test as far as the row being entered because there will never
-		// be a situation where a Queen is moved behind other Queens.
-		// Any further rows will all be empty.
-		for($i = 0; $i < $x; $i++)
-		{
-			// test the column to check for another Queen
-			if($this->board[$i][$y] === 1) return false;
-			
-			// Test the diagonals (backwards from the coordinate)
-			$tx = $x - 1 - $i;
-			$ty = $y - 1 - $i;	// diagonal this way \
-			if(($ty >= 0) && ($this->board[$tx][$ty] === 1)) return false;
-			
-			$ty = $y + 1 + $i;	// diagonal this way /
-			if(($ty < $n) && ($this->board[$tx][$ty] === 1)) return false;
-		}
-		
-		return true;
-	}
-	
-	// Rudimentary printing method
-	function printBoard()
-	{
-		for($row=0; $row<$this->numQueens; $row++)
-		{
-			$sep = '-';
-			for($col=0; $col<$this->numQueens; $col++)
-			{
-				$sep .= '--';	// for every column add 2 dashes to then print below the row
-				echo '|';
-				
-				$cell = $this->board[$row][$col];
-				if($cell === 1)
-				{
-					echo 'Q';
-				}
-				else
-				{
-					echo ' ';
-				}
-			}
-			
-			echo "|\n";
-			echo $sep . "\n";	// print the seperator row -------
-		}
-	}
+    /**
+     * @var int[]
+     */
+    private $result = [];
+
+    /**
+     * @var int
+     */
+    private $size;
+
+    /**
+     * Each column and diagonal is currently in use or not is in here.
+     * These prevents the need to manually check is row, column or diagonal.
+     * Much quicker!
+     *
+     * @var bool[][]
+     */
+    private $used = [
+        'column' => [],
+        'down' => [],
+        'up' => [],
+    ];
+
+    public function __construct(int $size)
+    {
+        $this->size = $size;
+        $this->result = array_fill(0, $size, null);
+    }
+
+    public function solve(int $queenNum = 0, $row = 0): bool
+    {
+        for($col = 0; $col < $this->size; $col++) {
+            $down = $this->size - 1 - $row + $col;
+            $up = $row + $col;
+
+            //echo "row: $row; col: $col; down: $down; up: $up",PHP_EOL;
+            if(($this->used['column'][$col] ?? false) === true
+                || ($this->used['down'][$down] ?? false) === true
+                || ($this->used['up'][$up] ?? false) === true
+            ) {
+                continue;
+            }
+
+            // if this cell is allowed, set the queen here
+            $this->result[$row] = $col;
+            $this->used['column'][$col] = true;
+            $this->used['down'][$down] = true;
+            $this->used['up'][$up] = true;
+
+
+            // If last queen or subsequent queens have been placed, return
+            if(($queenNum === $this->size - 1)
+                || $this->solve($queenNum + 1, $row + 1) === true
+            ) {
+                return true;
+            }
+
+            // otherwise, if we get here we've backtracked and have to try replacing this queen
+            $this->result[$row] = null;
+
+            $this->used['column'][$col] = false;
+            $this->used['down'][$down] = false;
+            $this->used['up'][$up] = false;
+        }
+
+        return false;
+    }
+
+    // Rudimentary printing method
+    public function display()
+    {
+        for($row = 0; $row < $this->size; $row++)
+        {
+            $sep = '-';
+            for($col = 0; $col < $this->size; $col++)
+            {
+                $sep .= '----';    // for every column add 4 dashes to then print below the row
+                echo '| ';
+
+                echo $this->result[$row] === $col ? 'Q ' : '  ';
+            }
+
+            echo "|\r\n";
+            echo $sep . "\n";    // print the seperator row -------
+        }
+    }
 }
 
 // Run main ...
-$queens = new Queens(8);
-$queens->solve(0, 0);
-
-$queens->printBoard();
-
-?>
+$queens = new Queens($_SERVER['argv'][1]);
+$queens->solve();
+$queens->display();
